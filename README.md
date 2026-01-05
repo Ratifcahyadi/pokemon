7c64a31dd8c969516d20e44531b2d5ff6da8bc3a
Ratifcahyadi <ratifcahyadi@students.amikom.ac.id>
2026-01-05 09:22:37 +0700

    project init

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to run this pokemon project
+
+- npm run dev
+- npx json-server data/db.json
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-5xl text-red-500">Text Test</h1>
+    <Card />
+  </div>
+</template>
+
+<script>
+  import Card from './components/Card.vue';
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
